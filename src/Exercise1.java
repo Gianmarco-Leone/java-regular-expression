59577c969cf45fd45bf33fbdfa3404301240ee63
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-10 10:04:31 +0200

    add ex1

--- src/Exercise1.java
+++ src/Exercise1.java
@@ -1,8 +1,21 @@
-// Write a regex that would match the following words: Dark, bark, Lark
-// For extra challenge, could you additionally make it match: stark
+// SCRIVI UNA REGEX IN GRADO DI MATCHARE CON QUESTE 3 PAROLE: Dark, bark, Lark
+// COME CHALLENGE AGGIUNGI IL MATCH CON: stark
 
 public class Exercise1 {
     public static void main(String[] args) {
+        String regex = "[DbL]ark";
 
+        System.out.println("Dark".matches(regex));
+        System.out.println("bark".matches(regex));
+        System.out.println("Lark".matches(regex));
+
+//        CHALLENGE
+        String regex2 = "(st|[DbL]|fgb)ark";
+
+        System.out.println("Dark".matches(regex2));
+        System.out.println("bark".matches(regex2));
+        System.out.println("Lark".matches(regex2));
+        System.out.println("stark".matches(regex2));
+        System.out.println("stark".matches(regex2));
     }
 }
