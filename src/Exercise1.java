2c4601267dca5a8a2af3e48d05092728b4badc37
Gianmarco-Leone <gianmarcoleone96@gmail.com>
2023-06-10 09:55:11 +0200

    init repo

---
+++ src/Exercise1.java
@@ -0,0 +1,8 @@
+// Write a regex that would match the following words: Dark, bark, Lark
+// For extra challenge, could you additionally make it match: stark
+
+public class Exercise1 {
+    public static void main(String[] args) {
+
+    }
+}
